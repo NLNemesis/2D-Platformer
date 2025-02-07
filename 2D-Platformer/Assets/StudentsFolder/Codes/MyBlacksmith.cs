872d035f4448eabd5c153b03c5181f15141ff6e2
Kost1ss <147918548+Kost1ss@users.noreply.github.com>
2025-02-07 16:02:18 +0200

    Changes

--- 2D-Platformer/Assets/StudentsFolder/Codes/MyBlacksmith.cs
+++ 2D-Platformer/Assets/StudentsFolder/Codes/MyBlacksmith.cs
@@ -12,18 +12,12 @@ public class MyBlacksmith : MonoBehaviour
     public GameObject ThisCanvas;
     private bool CanInteract;
 
-    [Header("Smelting")]
-    public string[] RequiredOre;
-    public int[] Payment;
-    public string[] SmeltingIngot;
-
-    [Header("Trading")]
-    public string[] RequiredItem;
-    public int[] ItemAmount;
-    public string[] GivenItem;
+    [Header("Shop")]
+    public string[] Item;
+    public int[] Value;
+    public bool[] Stackable;
 
     [Header("For UI Assignment")]
-    public string[] Item;
     public TextMeshProUGUI[] BuyItemAmount;
     public TextMeshProUGUI[] SellItemAmount;
     public TextMeshProUGUI GoldText;
@@ -79,23 +73,13 @@ public class MyBlacksmith : MonoBehaviour
         if (ThisCanvas.activeSelf == true)
         {
             GoldText.text = PM.Gold.ToString();
-            for (int i = 0; i < BuyItemAmount.Length; i++)
+            for (int i = 0; i < BuyItemAmount.Length; i++) 
             {
                 BuyItemAmount[i].text = PlayerItemsAmount[i].ToString();
                 SellItemAmount[i].text = PlayerItemsAmount[i].ToString();
             }
         }
         #endregion
-
-        if (Input.GetKeyDown(KeyCode.L))
-        {
-            Trading(0);
-        }
-
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            Trading(1);
-        }
     }
     #endregion
 
@@ -118,37 +102,28 @@ public class MyBlacksmith : MonoBehaviour
     }
     #endregion
 
-    #region Smelting
-    public void Smelting(int Number)
-    {
-        MI.CheckForItem(RequiredOre[Number]);
-
-        if (PM.Gold >= Payment[Number] && MI.ItemExists)
-        {
-            PM.Gold -= Payment[Number];
-            MI.RemoveItem(RequiredOre[Number]);
-            MI.AddItem(SmeltingIngot[Number], true);
-        }
-    }
-    #endregion
-
     #region Trading
+    [Header("Trading")]
+    public string[] PlayerItem; //Requested item from the player
+    public int[] RequestAmount; //Requested the amount
+    public string[] NPCItem;    //New player's item
+
     public void Trading(int Number)
     {
-        for (int i = 0; i < MI.ItemName.Length; i++) 
-        { 
-            if (MI.ItemName[i] == RequiredItem[Number] && MI.CarryAmount[i] >= ItemAmount[Number]) 
-            {
-                for (int j = 0; j < ItemAmount[Number]; j++)
-                {
-                    MI.RemoveItem(RequiredItem[Number]);
-                }
-                MI.AddItem(GivenItem[Number], false);
-                MI.RefreshInventoryVariables();
-                Debug.Log(i);
-                //MI.SortingSystem();
-                break;
-            }
+        int Amount = 0;
+
+        for (int i = 0; i < MI.SlotImage.Length; i++)
+        {
+            if (MI.SlotName[i] == PlayerItem[Number])
+            Amount += 1;
+        }  
+
+        if (Amount >= RequestAmount[Number])
+        {
+            for (int i = 0; i < RequestAmount[Number]; i++)
+            MI.RemoveItem(PlayerItem[Number]);
+
+            MI.AddItem(NPCItem[Number]);
         }
     }
     #endregion
@@ -157,16 +132,15 @@ public class MyBlacksmith : MonoBehaviour
     void FindItemsAmount()
     {
         PlayerItemsAmount = new int[Item.Length];
-        for (int i = 0; i < Item.Length; i++)
+        for (int i = 0; i < MI.SlotImage.Length; i++)
         {
-            for (int j = 0; j < MI.SlotImage.Length; j++)
+            for (int j = 0; j < Item.Length; j++)
             {
-                if (Item[i] == MI.SlotName[j])
+                if (MI.SlotName[i] == Item[j])
                 {
-                    if (MI.SlotAmount[j] == 0)
-                        PlayerItemsAmount[i] += 1;
-                    else
-                        PlayerItemsAmount[i] += MI.SlotAmount[j];
+                    PlayerItemsAmount[j] += 1;
+                    Debug.Log(Item[j] + " " + PlayerItemsAmount[j]);
+                    break;
                 }
             }
         }
--- 2D-Platformer/Assets/StudentsFolder/Codes/MyInventory.cs
+++ 2D-Platformer/Assets/StudentsFolder/Codes/MyInventory.cs
@@ -10,19 +10,15 @@ public class MyInventory : MonoBehaviour
     [Header("Game Items")]
     public Sprite[] ItemSprite;
     public string[] ItemName;
-    public int[] CarryAmount;
 
     [Header("Player Inventory")]
     public Image[] SlotImage;
     public TextMeshProUGUI[] AmountText;
     [HideInInspector] public bool[] SlotFull;
     [HideInInspector] public string[] SlotName;
-    public int[] SlotAmount;
-    public int StackableAmount;
     public Sprite EmptySprite;
     [HideInInspector] public int SlotAvailable;
     [HideInInspector] public bool ItemExists;
-    [HideInInspector] public int FoundSlot;
 
     [Header("Gold")]
     public int Gold;
@@ -30,17 +26,12 @@ public class MyInventory : MonoBehaviour
 
     [Header("References")]
     public PlayerMovement PM;
-
-    [Space(10)]
-    public string RemovableItem;
     #endregion
 
-    #region Start Update
     void Start()
     {
         SlotFull = new bool[SlotImage.Length];
         SlotName = new string[SlotImage.Length];
-        SlotAmount = new int[SlotImage.Length];
         SlotAvailable = SlotImage.Length + 1;
     }
 
@@ -51,52 +42,10 @@ public class MyInventory : MonoBehaviour
         #endregion
 
         GoldText.text = Gold.ToString();
-
-        if (Input.GetKeyDown(KeyCode.O))
-            RemoveItem(RemovableItem);
     }
-    #endregion
 
     #region Add Item
-    public void AddItem(string Name, bool Stackable)
-    {
-        if (Stackable)
-            AddingStackableItem(Name);     
-        else
-            AddingItem(Name, false);
-
-        RefreshInventoryVariables();
-    }
-
-    void AddingStackableItem(string Name)
-    {
-        for (int i = 0; i < SlotImage.Length; i++)
-        {
-            if (SlotFull[i] == true)
-            {
-                if (SlotName[i] == Name && SlotAmount[i] != StackableAmount)
-                {
-                    SlotAmount[i]++;
-                    AmountText[i].text = SlotAmount[i].ToString();
-
-                    if (SlotAmount[i] > StackableAmount)
-                    {
-                        SlotAmount[i]--;
-                        AmountText[i].text = SlotAmount[i].ToString();
-                        AddingItem(Name, true);
-                    }
-                    break;
-                }
-            }
-            else
-            {
-                AddingItem(Name, true);
-                break;
-            }
-        }
-    }
-
-    void AddingItem(string Name, bool Stackable)
+    public void AddItem(string Name)
     {
         for (int i = 0; i < SlotImage.Length; i++) 
         {
@@ -110,20 +59,12 @@ public class MyInventory : MonoBehaviour
                     if (ItemName[j] == Name)
                     {
                         SlotImage[i].sprite = ItemSprite[j];
-                        SlotAmount[i] = 1;
                         break;
                     }
-                }
-
-                if (Stackable)
-                {
-                    AmountText[i].gameObject.SetActive(true);
-                    AmountText[i].text = "1";
                 }
                 break;
             }
         }
-        RefreshInventoryVariables();
     }
     #endregion
 
@@ -134,57 +75,13 @@ public class MyInventory : MonoBehaviour
         {
             if (SlotName[i] == Name)
             {
-                if (AmountText[i].gameObject.activeSelf)
-                {
-                    if (SlotAmount[i] > 1)
-                    {
-                        SlotAmount[i]--;
-                        AmountText[i].text = SlotAmount[i].ToString();
-
-                        if (SlotAmount[i] == 0)
-                        {
-                            AmountText[i].gameObject.SetActive(false);
-                            AmountText[i].text = "0";
-                            RemovingItem(Name);
-                        }
-                        break;
-                    }
-                    else
-                    {
-                        AmountText[i].gameObject.SetActive(false);
-                        AmountText[i].text = "0";
-                        RemovingItem(Name);
-                        break;
-                    }
-                }
-                else
-                {
-                    RemovingItem(Name);
-                    break;
-                }
-            }
-        }
-
-        RefreshInventoryVariables();
-    }
-
-    void RemovingItem(string Name)
-    {
-        for (int i = 0; i < SlotImage.Length; i++)
-        {
-            if (SlotName[i] == Name)
-            {
                 SlotImage[i].sprite = EmptySprite;
                 SlotFull[i] = false;
                 SlotName[i] = "Empty";
                 SlotAvailable++;
-                SlotAmount[i] = 0;
-                AmountText[i].gameObject.SetActive(false);
-                AmountText[i].text = "0";
                 break;
             }
         }
-        RefreshInventoryVariables();
     }
     #endregion
 
@@ -192,13 +89,11 @@ public class MyInventory : MonoBehaviour
     public void CheckForItem(string Name)
     {
         ItemExists = false;
-        FoundSlot = -1;
         for (int i = 0; i < SlotImage.Length; i++)
         {
             if (SlotName[i] == Name)
             {
                 ItemExists = true;
-                FoundSlot = i;
                 break;
             }
         }
@@ -235,61 +130,6 @@ public class MyInventory : MonoBehaviour
         SlotFull[Number] = false;
         SlotName[Number] = "Empty";
         SlotAvailable++;
-        SlotAmount[Number] = 0;
-        AmountText[Number].gameObject.SetActive(false);
-        AmountText[Number].text = "0";
-    }
-    #endregion
-
-    #region Refresh Inventory & Sorting System
-    public void RefreshInventoryVariables()
-    {
-        CarryAmount = new int[ItemName.Length];
-        for (int i = 0; i < CarryAmount.Length; i++)
-        {
-            for (int j = 0; j < SlotImage.Length; j++)
-            {
-                if (ItemName[i] == SlotName[j])
-                {
-                    if (SlotAmount[j] == 0)
-                        CarryAmount[i]++;
-                    else
-                        CarryAmount[i] += SlotAmount[j];
-                }
-            }
-        }
-    }
-
-    public void SortingSystem()
-    {
-        string[] SavedSlotName = new string[SlotImage.Length];
-        SavedSlotName = SlotName;
-        int[] SavedSlotAmount = new int [SlotImage.Length];
-        SavedSlotAmount = SlotAmount;
-        
-        for (int i = 0; i < SlotImage.Length; i++)
-        {
-            SlotImage[i].sprite = EmptySprite;
-            SlotFull[i] = false;
-            SlotName[i] = "Empty";
-            SlotAvailable++;
-            SlotAmount[i] = 0;
-            AmountText[i].gameObject.SetActive(false);
-            AmountText[i].text = "0";
-        }
-
-        for (int i = 0; i < SlotImage.Length; i++)
-        {
-            if (SavedSlotAmount[i] > 1)
-            {
-                for (int j = 0; j < SavedSlotAmount[i]; j++)
-                {
-                    AddItem(SavedSlotName[i], true);
-                }
-            }
-            else if (SavedSlotAmount[i] == 1)
-                AddItem(SavedSlotName[i], false);
-        }
     }
     #endregion
 }
