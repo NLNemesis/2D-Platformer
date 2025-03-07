0f1f627349e097df9255d6004a3be93e2775237a
TachoTelis <147918446+TachoTelis@users.noreply.github.com>
2025-03-07 17:47:26 +0200

    Set

--- 2D-Platformer/Assets/StudentsFolder/Codes/Gacha.cs
+++ 2D-Platformer/Assets/StudentsFolder/Codes/Gacha.cs
@@ -8,76 +8,27 @@ using TMPro;
 public class Gacha : MonoBehaviour
 {
     #region Variables
-    [Header("Items")]
+    [Header("Wish Rewards")]
     public string MainFiveStar;
     public string[] FiveStar;
     public string[] FourStar;
     public string[] ThreeStar;
 
-    [Header("Wishes")]
+    [Header("Controller")]
+    public int WishCost;
     private int Pity;
-    private int CurrentPull;
+    private int CurrentPity;
     private bool Guarantee;
 
     [Header("References")]
-    public PlayerMovement PM;
     public MyInventory MI;
     #endregion
 
     public void BuyWish()
     {
-        if (PM.Gold >= 1000 && MI.SlotAvailable > 0)
+        if (MI.SlotAvailable > 0 && MI.Gold >= WishCost)
         {
-            PM.Gold -= 1000;
-            MI.AddItem("Wish");
-        }
-    }
 
-    public void UseWish()
-    {
-        if(MI.SlotAvailable > 0)
-        {
-            MI.CheckForItem("Wish");
-            if (MI.ItemExists == true)
-            {
-                MI.RemoveItem("Wish");
-                Pity += 1;
-                CurrentPull += 1;
-                //Try to pull the five star weapon
-                int Number = Random.Range(0, 91);
-                if (Pity >= Number) //Takes the five star weapon
-                {
-                    Pity = 0;
-                    if (Guarantee) //If the player has guarantee the banner's five star weapon
-                    {
-                        Guarantee = false;
-                        MI.AddItem(MainFiveStar);
-                    }
-                    else
-                    {
-                        Number = Random.Range(0, 2);
-                        if (Number == 0)
-                            MI.AddItem(MainFiveStar);
-                        else
-                        {
-                            Guarantee = true;
-                            Number = Random.Range(0, FiveStar.Length);
-                            MI.AddItem(FiveStar[Number]);
-                        }
-                    }
-                }
-                else if (CurrentPull >= 10) //Takes the four star weapon
-                {
-                    CurrentPull = 0;
-                    Number = Random.Range(0, FourStar.Length);
-                    MI.AddItem(FourStar[Number]);
-                }
-                else //Takes the three star weapon
-                {
-                    Number = Random.Range(0, ThreeStar.Length);
-                    MI.AddItem(ThreeStar[Number]);
-                }
-            }
         }
     }
 }
