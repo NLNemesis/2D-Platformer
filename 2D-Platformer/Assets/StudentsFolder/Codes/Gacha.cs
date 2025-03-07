6d09fede7df7b4c8056ffa9f3faa80fbe96c573d
IndieTeacher <UnitysEducation@gmail.com>
2025-03-07 20:02:58 +0200

    Changes

--- 2D-Platformer/Assets/StudentsFolder/Codes/Gacha.cs
+++ 2D-Platformer/Assets/StudentsFolder/Codes/Gacha.cs
@@ -22,13 +22,62 @@ public class Gacha : MonoBehaviour
 
     [Header("References")]
     public MyInventory MI;
+    public TextMeshProUGUI[] UIText;
     #endregion
 
+    void Update()
+    {
+
+    }
+
+    void CountWishes()
+    {
+
+    }
+    #region Wish System
     public void BuyWish()
     {
         if (MI.SlotAvailable > 0 && MI.Gold >= WishCost)
         {
-
+            MI.Gold -= WishCost;
+            MI.AddItem("Wish");
         }
     }
+
+    public void Wish()
+    {
+        MI.CheckForItem("Wish");
+        if (MI.ItemExists)
+        {
+            MI.RemoveItem("Wish");
+            Pity++;
+            CurrentPity++;
+            int Number = Random.Range(0, 91);
+            if (Pity >= Number)
+            {
+                Pity = 0;
+                if (Guarantee)
+                {
+                    Guarantee = false;
+                    MI.AddItem(MainFiveStar);
+                }
+                else
+                {
+                    
+                }
+            }
+            else if (CurrentPity >= 10)
+            {
+                CurrentPity = 0;
+                Number = Random.Range(0, FourStar.Length);
+                MI.AddItem(FourStar[Number]);
+            }
+            else
+            {
+                Number = Random.Range(0, ThreeStar.Length);
+                MI.AddItem(ThreeStar[Number]);
+            }
+        }
+    }
+    #endregion
 }
