e6853422bc4f912fdb609b26e52e8b85058a1094
IndieTeacher <UnitysEducation@gmail.com>
2025-04-04 18:45:04 +0300

    Change

--- 2D-Platformer/Assets/StudentsFolder/Codes/Enemy/Enemy.cs
+++ 2D-Platformer/Assets/StudentsFolder/Codes/Enemy/Enemy.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    #region Variables
+    [Header("Stats")]
+    public float Health;
+    public float Armor;
+    public float MR;
+    #endregion
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +20,22 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void TakeDamage(float Value, bool Magic)
+    {
+        float Damage = 0;
+        if (Magic == true)
+            Damage = Value - MR;
+        else
+            Damage = Value - Armor;
+
+        if (Damage > 0)
+        {
+            if (Health - Damage > 0)
+                Health -= Damage;
+            else
+                this.gameObject.SetActive(false);
+        }
     }
 }
