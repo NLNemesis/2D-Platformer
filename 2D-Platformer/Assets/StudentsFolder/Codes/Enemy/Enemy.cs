cb2553f749569074ee1828cdaa474743af381118
IndieTeacher <UnitysEducation@gmail.com>
2025-04-11 20:36:29 +0300

    Animation

--- 2D-Platformer/Assets/StudentsFolder/Codes/Enemy/Enemy.cs
+++ 2D-Platformer/Assets/StudentsFolder/Codes/Enemy/Enemy.cs
@@ -13,6 +13,7 @@ public class Enemy : MonoBehaviour
     public float Speed;
     private int Direction;
     private float Distance;
+    private bool CanMove = true;
 
     [Header("Stats")]
     public float Health;
@@ -22,17 +23,25 @@ public class Enemy : MonoBehaviour
     [Header("References")]
     private Animator animator;
     #endregion
-
+        
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        animator.SetFloat("State", 1);
+        this.transform.localScale = Point[0].localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (CanMove && Type == EnemyType.Classic)
+        {
+            Distance = (this.transform.position - Point[Direction].position).magnitude;
+            this.transform.position = Vector2.MoveTowards(this.transform.position, Point[Direction].position, Speed);
+            if (Distance == 0)
+                StartCoroutine(ChangeDirection());
+        }
     }
     #region Take Damage
     public void TakeDamage(float Value, bool Magic)
@@ -52,4 +61,22 @@ public class Enemy : MonoBehaviour
             animator.SetTrigger("Death");
     }
     #endregion
+    IEnumerator ChangeDirection()
+    {
+        CanMove = false;
+        animator.SetFloat("State", 0);
+        yield return new WaitForSeconds(3f);
+        if (Direction == 0)
+        {
+            Direction = 1;
+            this.transform.localScale = Point[Direction].localScale;
+        }
+        else
+        {
+            Direction = 0;
+            this.transform.localScale = Point[Direction].localScale;
+        }
+        animator.SetFloat("State", 1);
+        CanMove = true;
+    }
 }
