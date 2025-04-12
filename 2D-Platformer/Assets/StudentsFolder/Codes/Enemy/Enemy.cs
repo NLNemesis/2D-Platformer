d5d9e13cf37203b988c073dbdad94ab0bb58f8fe
KG_Zanie <77167473+NLNemesis@users.noreply.github.com>
2025-04-12 07:42:14 +0300

    Changes

--- 2D-Platformer/Assets/StudentsFolder/Codes/Enemy/Enemy.cs
+++ 2D-Platformer/Assets/StudentsFolder/Codes/Enemy/Enemy.cs
@@ -5,16 +5,6 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     #region Variables
-    public enum EnemyType {Dummy,Classic,Boss};
-    public EnemyType Type;
-
-    [Header("Patrol")]
-    public Transform[] Point;
-    public float Speed;
-    private int Direction;
-    private float Distance;
-    private bool CanMove = true;
-
     [Header("Stats")]
     public float Health;
     public float Armor;
@@ -28,21 +18,14 @@ public class Enemy : MonoBehaviour
     void Start()
     {
         animator = GetComponent<Animator>();
-        animator.SetFloat("State", 1);
-        this.transform.localScale = Point[0].localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (CanMove && Type == EnemyType.Classic)
-        {
-            Distance = (this.transform.position - Point[Direction].position).magnitude;
-            this.transform.position = Vector2.MoveTowards(this.transform.position, Point[Direction].position, Speed);
-            if (Distance == 0)
-                StartCoroutine(ChangeDirection());
-        }
+
     }
+
     #region Take Damage
     public void TakeDamage(float Value, bool Magic)
     {
@@ -61,22 +44,4 @@ public class Enemy : MonoBehaviour
             animator.SetTrigger("Death");
     }
     #endregion
-    IEnumerator ChangeDirection()
-    {
-        CanMove = false;
-        animator.SetFloat("State", 0);
-        yield return new WaitForSeconds(3f);
-        if (Direction == 0)
-        {
-            Direction = 1;
-            this.transform.localScale = Point[Direction].localScale;
-        }
-        else
-        {
-            Direction = 0;
-            this.transform.localScale = Point[Direction].localScale;
-        }
-        animator.SetFloat("State", 1);
-        CanMove = true;
-    }
 }
