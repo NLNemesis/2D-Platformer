3705b4e65f333f46363980d99e0059874c583eba
KG_Zanie <77167473+NLNemesis@users.noreply.github.com>
2025-05-09 14:34:01 +0300

    Main Menu

---
+++ 2D-Platformer/Assets/Scenes/MainMenu/MainMenu.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    #region Variables
+    [Header("Introduction")]
+    public float IntroDelay;
+
+    [Header("References")]
+    public Animator animator; //MainMenuAnimator
+
+    [Header("Events")]
+    public UnityEvent IntroductionEvent;
+    public UnityEvent StartEvent;
+    public UnityEvent QuitEvent;
+    #endregion
+
+    #region When the game loads
+    void Start()
+    {
+        StartCoroutine(IntroductionDelay());
+    }
+
+    IEnumerator IntroductionDelay()
+    {
+        yield return new WaitForSeconds(IntroDelay);
+        IntroductionEvent.Invoke();
+    }
+    #endregion
+
+    void Update()
+    {
+        
+    }
+
+    public void ShowMainButtons() { animator.SetTrigger("ShowMainButtons"); }
+
+    #region Start Button
+    public void StartButton()
+    {
+        animator.SetTrigger("Return");
+        StartCoroutine(StartEventTrigger());
+    }
+
+    IEnumerator StartEventTrigger()
+    {
+        yield return new WaitForSeconds(1f);
+        StartEvent.Invoke();
+    }
+
+    public void ToNewLevel() { SceneManager.LoadScene(1); }
+    #endregion
+
+    #region Quit Button
+    public void QuitButton() 
+    {
+        animator.SetTrigger("Return");
+        StartCoroutine(QuitEventTrigger());
+    }
+
+    IEnumerator QuitEventTrigger()
+    {
+        yield return new WaitForSeconds(1f);
+        QuitEvent.Invoke();
+    }
+
+    public void CloseTheApplication() { Application.Quit(); }
+    #endregion
+}
