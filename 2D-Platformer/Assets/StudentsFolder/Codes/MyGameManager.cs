03cb4eddac8a2ddd4fdc4943170428aa9931c69b
IndieTeacher <UnitysEducation@gmail.com>
2025-05-10 11:04:25 +0300

    Changes

--- 2D-Platformer/Assets/StudentsFolder/Codes/MyGameManager.cs
+++ 2D-Platformer/Assets/StudentsFolder/Codes/MyGameManager.cs
@@ -9,12 +9,17 @@ public class MyGameManager : MonoBehaviour
     #region Variables
     public int UIOpened;
     public GameObject InventoryObject;
+
+    [Header("Pause Menu")]
+    public UnityEvent OnPause;
+    public UnityEvent OnResume;
+    public GameObject[] DisableObject;
+    private bool[] ObjectActivity;
     #endregion
 
-    // Start is called before the first frame update
     void Start()
     {
-
+        ObjectActivity = new bool[DisableObject.Length];
     }
 
     // Update is called once per frame
@@ -22,6 +27,9 @@ public class MyGameManager : MonoBehaviour
     {
         if (UIOpened == 0 || UIOpened == 2)
             InventoryController();
+
+        if (UIOpened == 0 || UIOpened == 1)
+            PauseMenu();
     }
 
     void InventoryController()
@@ -55,5 +63,46 @@ public class MyGameManager : MonoBehaviour
             UIOpened = 0;
         }
         #endregion
+    }
+
+    #region Pause Menu Controller
+    public void PauseMenu()
+    { 
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (UIOpened == 0)
+            {
+                OnPause.Invoke();
+                UIOpened = 1;
+                for (int i = 0; i < DisableObject.Length; i++)
+                {
+                    ObjectActivity[i] = DisableObject[i].activeSelf;
+                    DisableObject[i].SetActive(false);
+                }
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+                Time.timeScale = 0;
+            }
+            else
+                Resume();
+        }
+    }
+
+    public void Resume()
+    {
+        OnResume.Invoke();
+        UIOpened = 0;
+        for (int i = 0; i < DisableObject.Length; i++)
+            DisableObject[i].SetActive(ObjectActivity[i]);
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Time.timeScale = 1;
+    }
+    #endregion
+
+    public void ChangeLevel(int Number)
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(Number);
     }
 }
