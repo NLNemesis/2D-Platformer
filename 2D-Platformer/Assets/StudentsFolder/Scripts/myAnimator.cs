38b6a95f976320ee06a8491aa65d89a1767488db
IndieTeacher <UnitysEducation@gmail.com>
2025-11-08 11:48:37 +0200

    Reset

--- 2D-Platformer/Assets/StudentsFolder/Scripts/myAnimator.cs
+++ 2D-Platformer/Assets/StudentsFolder/Scripts/myAnimator.cs
@@ -4,17 +4,8 @@ using UnityEngine;
 
 public class myAnimator : MonoBehaviour
 {
-<<<<<<< HEAD
     private Animator animator;
     private myPlayer player;
-
-=======
-    public Animator animator;
-    public myPlayer player;
-<<<<<<< Updated upstream
-=======
->>>>>>> 0ff38a3bfa8dfd332af1a923e402f0b70d7207a6
->>>>>>> Stashed changes
     // Start is called before the first frame update
     void Start()
     {
@@ -25,32 +16,13 @@ public class myAnimator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
         if (!player.frozen)
         {
             if (Input.GetMouseButtonDown(0))
             {
                 animator.Play("Attack");
                 player.Freeze();
-=======
->>>>>>> Stashed changes
-        if (player.inAir == false)
-        {
-            if (Input.GetButton("Move_Buttons") || Input.GetButton("Move_Arrows"))
-                animator.Play("Move");
-            else
-                animator.Play("Idle");
 
-            if (Input.GetButtonDown("Jump"))
-            {
-                player.inAir = true;
-                animator.Play("Jump");
-<<<<<<< Updated upstream
-=======
->>>>>>> 0ff38a3bfa8dfd332af1a923e402f0b70d7207a6
->>>>>>> Stashed changes
             }
         }
     }
