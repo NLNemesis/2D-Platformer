4f861ad9ae592cabd2c9d5e8daccf712d2fca295
IndieTeacher <UnitysEducation@gmail.com>
2025-12-20 12:40:41 +0200

    Reset

--- 2D-Platformer/Assets/StudentsFolder/Scripts/EnemyDetect.cs
+++ 2D-Platformer/Assets/StudentsFolder/Scripts/EnemyDetect.cs
@@ -8,20 +8,20 @@ public class EnemyDetect : MonoBehaviour
     public int damage;
     public myPlayer player;
     public myEnemy enemy;
+
+    private bool dealt;
     #endregion
     private void OnTriggerStay2D(Collider2D Object)
     {
-
+        if (Object.name == "Player" && !dealt)
+        {
+            dealt = true;
+            player.LoseHP(damage);
+        }
     }
     // Start is called before the first frame update
-    void Start()
+    void OnEnable()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        dealt = false;
     }
 }
