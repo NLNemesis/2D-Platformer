5dfb9363c991f698a552278bd60d8a227052d004
IndieTeacher <UnitysEducation@gmail.com>
2025-12-20 12:25:39 +0200

    Reset

---
+++ 2D-Platformer/Assets/StudentsFolder/Scripts/EnemyDetect.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDetect : MonoBehaviour
+{
+    #region Variables
+    public int damage;
+    public myPlayer player;
+    public myEnemy enemy;
+    #endregion
+    private void OnTriggerStay2D(Collider2D Object)
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
