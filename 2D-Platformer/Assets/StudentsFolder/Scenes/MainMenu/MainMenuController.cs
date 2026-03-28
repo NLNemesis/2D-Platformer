00085e38595da671a18f775fde51d6f1ee4671a4
IndieTeacher <UnitysEducation@gmail.com>
2026-03-28 13:04:58 +0200

    Menu

---
+++ 2D-Platformer/Assets/StudentsFolder/Scenes/MainMenu/MainMenuController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public Animator animator;
+
+    #region Start Button
+    public void StartButton()
+    {
+        StartCoroutine(StartGame());
+    }
+
+    IEnumerator StartGame()
+    {
+        animator.Play("Buttons_Reverse");
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene(1);
+    }
+    #endregion
+
+    #region Quit Button
+    public void QuitButton()
+    {
+        StartCoroutine(QuitGame());
+    }
+
+    IEnumerator QuitGame()
+    {
+        animator.Play("Buttons_Reverse");
+        yield return new WaitForSeconds(3f);
+        Application.Quit();
+    }
+    #endregion
+
+}
