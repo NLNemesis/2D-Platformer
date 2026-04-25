d360e1b663f0277f568da8fb137a23e151de3dca
IndieTeacher <UnitysEducation@gmail.com>
2026-04-25 13:22:41 +0300

    Change

--- 2D-Platformer/Assets/SaveSystem/SaveGameController.cs
+++ 2D-Platformer/Assets/SaveSystem/SaveGameController.cs
@@ -6,4 +6,23 @@ public class SaveGameController : MonoBehaviour
 {
     public bool InMenu;
     public MainMenuController mmc;
+
+    private void Awake()
+    {
+        Settings s = SaveSystem.LoadSettings();
+        if (s != null)
+        {
+            mmc.master = s.master;
+            mmc.sfx = s.sfx;
+            mmc.ambient = s.ambient;
+            mmc.Set_Master_Volume(s.master);
+            mmc.Set_Sfx_Volume(s.sfx);
+            mmc.Set_Ambient_Volume(s.ambient);
+        }
+        else
+        {
+            Debug.Log("Save settings not found");
+            SaveSystem.SaveSettings(this);
+        }
+    }
 }
